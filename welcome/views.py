c5397c35b0e81e22477d46fd4ad4f0cfa80b3e0e
omengoemmanuel <omengomanuel@gmail.com>
2024-08-29 18:19:20 +0300

    project customizing

---
+++ welcome/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
