89b3be0a14fda4aacfddf8ba5bc1bd5f1829326b
omengoemmanuel <omengomanuel@gmail.com>
2024-09-03 16:09:08 +0300

    services price category

---
+++ admins/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class survey(models.Model):
+    client_category = models.CharField(max_length=20, null=False, blank=False)
+    survey_fees = models.PositiveIntegerField(null=False, blank=False)
+    local_authority_fees = models.PositiveIntegerField(null=False, blank=False)
+    survey_photo = models.ImageField(upload_to="uploads/survey", default="uploads/survey/photo.jpg")
+
+    def __str__(self):
+        return self.client_category
