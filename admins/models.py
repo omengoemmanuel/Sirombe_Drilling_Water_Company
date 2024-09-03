98729aee676b53eb0aefb57eb455606c24bc7dba
omengoemmanuel <omengomanuel@gmail.com>
2024-09-03 16:14:52 +0300

    services price category

--- admins/models.py
+++ admins/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 # Create your models here.
 class survey(models.Model):
-    client_category = models.CharField(max_length=20, null=False, blank=False)
+    client_choices = [
+        ('Industrial', 'Industrial'),
+        ('Commercial', 'Commercial'),
+        ('Domestic', 'Domestic')
+    ]
+    client_category = models.CharField(max_length=20, null=False, blank=False, choices=client_choices)
     survey_fees = models.PositiveIntegerField(null=False, blank=False)
     local_authority_fees = models.PositiveIntegerField(null=False, blank=False)
     survey_photo = models.ImageField(upload_to="uploads/survey", default="uploads/survey/photo.jpg")
