9de834756c78893ef735b72c7f667da4c262cc51
omengoemmanuel <omengomanuel@gmail.com>
2024-09-05 18:53:59 +0300

    authenications_edits

--- admins/urls.py
+++ admins/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('signin', views.signin, name="signin"),
     path('signup', views.signup, name="signup"),
-    path('signout', views.signout, name="signout")
+    path('signout', views.signout, name="signout"),
+    path('home', views.home, name="home")
 
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,15 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import survey
+
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been created successfully")
+
+        return redirect('signin')
+
     return render(request, 'signup.html')
 
 
 def signin(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid Logins")
+            return redirect("signin")
+
+    return render(request, 'signin.html')
 
 
 def signout(request):
     pass
+
+
+def home(request):
+    return render(request, 'home.html')
